b8626edc62db599af71beeb5aada81b34ab03cb4
joseluisvazzquez <jvazquez3@nigul.cide.es>
2024-03-11 15:46:56 +0100

    pages ¡

--- page/src/Navigation.tsx
+++ page/src/Navigation.tsx
@@ -10,10 +10,10 @@ function Navigation(){
         <Container fluid>
           <Navbar.Brand as={Link} to="/"><img src={ico} style={{width:"50px", height:"50px"}} /></Navbar.Brand>
           <Nav className="justify-content-center">
-          <Link to="/Hotspots"  className='me-5 display-6 text-decoration-none link-danger'>Hotspots</Link>
-            <Link to="/Cuisine"   className='me-5 display-6 text-decoration-none link-danger'>Cuisine</Link>
-            <Link to="/Lodging" className='me-5 display-6 text-decoration-none link-danger'>Lodging</Link>
-            <Link to="/Activities"  className='me-5 display-6 text-decoration-none link-danger'>Activities</Link>
+          <Link to="/Hotspots"  className='me-5 h-3 lead text-decoration-none link-danger'>Hotspots</Link>
+            <Link to="/Cuisine"   className='me-5 h-3 lead text-decoration-none link-danger'>Cuisine</Link>
+            <Link to="/Lodging" className='me-5 h-3 lead text-decoration-none link-danger'>Lodging</Link>
+            <Link to="/Activities"  className='me-5 h-3 lead text-decoration-none link-danger'>Activities</Link>
           </Nav>
         </Container>
       </Navbar>
